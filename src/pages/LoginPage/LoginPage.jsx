bc23239c768743595087b1f18ad1a50fa0757683
jshprintz <jshprintz@gmail.com>
2022-09-23 15:06:17 -0700

    added handleCHange and handleSubmit functions for login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,8 +2,40 @@ import React, { useState } from "react";
 import "./LoginPage.css";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import userService from "../../utils/userService";
+import { useNavigate, Link } from "react-router-dom";
+
 
 export default function LoginPage(props) {
+  const [error, setError] = useState('');
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+  });
+
+const navigate = useNavigate();
+
+function handleChange(e) {
+  setState({
+    ...state,
+    [e.target.name]: e.target.value,
+  });
+}
+
+async function handleSubmit(e) {
+  e.preventDefault();
+
+  try {
+    await userService.login(state);
+    props.handleSignUpOrLogin();
+    navigate('/');
+
+  } catch(err) {
+    console.log(err, "<--ERROR IN LOGIN PAGE")
+    setError(err.message);
+  }
+}
+
+
   return (
     <>
       <h1>Setup Login Page</h1>
