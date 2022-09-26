c577bd8d04118e54b8967632f755f35961cf1a61
jshprintz <jshprintz@gmail.com>
2022-09-26 10:46:34 -0700

    added logic based on logged in user whether a follow icon or a delete icon would appear.

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -10,11 +10,15 @@ function PostCard({ post, isProfile, addLike, removeLike, loggedUser }) {
     (like) => like.username === loggedUser.username
   );
   const likeColor = likedIndex > -1 ? "red" : "grey";
-                     
+
   const likeClickHandler =
     likedIndex > -1
       ? () => removeLike(post.likes[likedIndex]._id) 
       : () => addLike(post._id);  
+
+
+  console.log(loggedUser.username, "<<<-Logged user")
+  console.log(post.user.username)
 
 //--------------------------------------------------------
 //  FOLLOW. PROBABLY WILL NEED TO CHANGE THE USER MODEL.
@@ -35,26 +39,7 @@ function PostCard({ post, isProfile, addLike, removeLike, loggedUser }) {
     <Card key={post._id} >
 
       <Card.Content>
-        <Card.Header>{post.title}</Card.Header>
-        <p>{post.poem}</p>
-      </Card.Content>
-
-
-      <Card.Content textAlign={"right"}>
-
-        {isProfile ? (
-
-        <Link to={`#`}>
-        <Icon
-        name={"delete"}
-        size="large"
-        //color={"followColor"}
-        //onClick={followClickHandler}
-        />
-        </Link>
-      ) : (
-        <>
-        <Card.Header textAlign="left">
+      <Card.Header textAlign="center">
             <Link to={`/${post.user.username}`}>
               <Image
                 size="large"
@@ -68,6 +53,27 @@ function PostCard({ post, isProfile, addLike, removeLike, loggedUser }) {
               {post.user.username}
             </Link>
           </Card.Header>
+                <br />
+        <Card.Header>{post.title}</Card.Header>
+        <p>{post.poem}</p>
+      </Card.Content>
+
+
+      <Card.Content textAlign={"right"}>
+
+        {post.user.username === loggedUser.username ? (
+
+        <Link to={`#`}>
+        <Icon
+        name={"delete"}
+        size="large"
+        //color={"followColor"}
+        //onClick={followClickHandler}
+        />
+        </Link>
+        
+      ) : (
+        <>
 
         <Icon
           name={"heart"}
